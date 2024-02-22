2fc122efb62dd733ebcd2b48bc835156fb267802
pipeduque <juan10duque@gmail.com>
2024-02-22 13:47:31 -0500

    agregar casos de prueba

--- src/main.py
+++ src/main.py
@@ -13,7 +13,6 @@ states = [
     [1, 1, 1],
 ]
 
-# Lista de probabilidades del sistema
 probabilities = [
     [1, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 1, 0, 0, 0],
@@ -37,8 +36,45 @@ cs_value = [1, 0, 0]
 decomposition(ns, cs, cs_value, probabilities, states)
 cut_process(ns, cs, cs_value, probabilities, states)
 
+"""
+# pruebas decomposition
+# Ejemplo 1 proyecto
+ns = "ABC"
+cs = "ABC"
+cs_value = [1, 0, 0]
 
-# test decomposition
+decomposition(ns, cs, cs_value)
+
+# Ejemplo 2 proyecto
+ns = "AB"
+cs = "ABC"
+cs_value = [1, 0, 0]
+
+decomposition(ns, cs, cs_value)
+
+# Ejemplo 3 proyecto
+ns = "AC"
+cs = "ABC"
+cs_value = [1, 0, 0]
+
+decomposition(ns, cs, cs_value)
+
+# Ejemplo 4 proyecto
+ns = "A"
+cs = "AC"
+cs_value = [1, 0, 0]
+
+decomposition(ns, cs, cs_value)
+
+# Ejemplo 5 proyecto
+ns = "ABC"
+cs = "AB"
+cs_value = [1, 0, 0]
+
+decomposition(ns, cs, cs_value)
+
+
+# pruebas cut
 states = [
     [0, 0, 0, 0, 0, 0],
     [1, 0, 0, 0, 0, 0],
@@ -174,5 +210,65 @@ probabilities = [
     [1, 1, 0, 0, 1, 0],
 ]
 
-
 cs_value = [1, 0, 0, 0, 1, 0]
+
+# A
+ns = "ABC"
+cs = "ABC"
+
+cut_process(ns, cs, cs_value, probabilities, states)
+
+# B
+ns = "B"
+cs = "A"
+
+cut_process(ns, cs, cs_value, probabilities, states)
+
+# C
+ns = "A"
+cs = "B"
+
+cut_process(ns, cs, cs_value, probabilities, states)
+
+# D
+ns = "C"
+cs = "ABC"
+
+cut_process(ns, cs, cs_value, probabilities, states)
+
+# E
+ns = "AB"
+cs = "C"
+
+cut_process(ns, cs, cs_value, probabilities, states)
+
+# F
+ns = "ABCD"
+cs = "ABCD"
+
+cut_process(ns, cs, cs_value, probabilities, states)
+
+# G
+ns = "ABCDEF"
+cs = "ABCD"
+
+cut_process(ns, cs, cs_value, probabilities, states)
+
+# H
+ns = "ABCDE"
+cs = "ABC"
+
+cut_process(ns, cs, cs_value, probabilities, states)
+
+# H
+ns = "ABCDE"
+cs = "ABC"
+
+cut_process(ns, cs, cs_value, probabilities, states)
+
+# J
+ns = "BC"
+cs = "ABC"
+
+cut_process(ns, cs, cs_value, probabilities, states)
+"""
